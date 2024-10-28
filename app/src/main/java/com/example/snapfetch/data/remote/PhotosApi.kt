e1e393d545dcc60c94d006ea780469f77b665ce1
Artur Wilczek <aw.wilczek@gmail.com>
2024-10-28 22:10:00 +0100

    skip launch icon display

--- app/src/main/java/com/example/snapfetch/data/remote/PhotosApi.kt
+++ app/src/main/java/com/example/snapfetch/data/remote/PhotosApi.kt
@@ -4,6 +4,11 @@ import com.example.snapfetch.data.remote.dtos.PhotoDto
 import retrofit2.http.GET
 
 interface PhotosApi {
+
     @GET("v2/list")
     suspend fun getPhotos(): List<PhotoDto>
+
+//    @GET()
+//    suspend fun getPhotoDetails(photoId: String): PhotoDetailsDto
+
 }
